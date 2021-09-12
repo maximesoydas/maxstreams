bb4196e479e07e9b326e55fc3e5ee9b17c91b5c2
Maxime Soydas <max.soydas@gmail.com>
2021-09-13 01:59:07 +0200

    fix

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-// MaxStream LOGIC
-
-    // Create url categories (2 urls/pages per category)
-    // Loop Over each url in URLS_CATEGORY (2 urls) (5movies per page)
-    // Return the data of 7 movies from 2 urls in a single object
-    // Remove the last two movies of the second url
-    // Loop Over Each data/movie
-    // create div element for each movie
-    // add data/movie image to element
-    // add button to trigger modal box
-    // add data/movie info to modal box
-    // repeat for 4 categories total
-    // add best THE highest scored imdb movie
+const scoreSliders = document.querySelector(".carouselbox")
+const actionSliders = document.querySelector(".action-carouselbox")
+const animationSliders = document.querySelector(".animation-carouselbox")
+const dramaSliders = document.querySelector(".drama-carouselbox")
+const bestMovie = document.querySelector(".best-movie")
+var scrollPerClick;
+var ImagePadding = 20;
 
 
 
-// Create url categories (2 urls/pages per category)
+// STORE MOVIES IN DIFFRENT CATEGORIES LIST//
 
-
-// Initialize the lists
 const score_category = []
 const animation_category = []
 const action_category = []
-const bio_category = []
+const drama_category = []
 
-// Append 2 urls per category to the corresponding category list
 for (let i = 1; i < 3; i++) {
     //Best Score Category
     let SCORE_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=+-imdb_score&title=&title_contains=&writer=&writer_contains=&year=`;
@@ -35,277 +26,215 @@ for (let i = 1; i < 3; i++) {
     let ACTION_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Action&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
     action_category.push(ACTION_URL)
     // Biograhy Category
-    let BIO_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Biography&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
-    bio_category.push(BIO_URL)
+    let DRAMA_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Drama&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
+    drama_category.push(DRAMA_URL)
 }
 
-// Loop Over each url in URLS_CATEGORY (2 urls)
 
-score_url_obj1 = {}
-score_url_obj2 = {}
+// CALL THE SCORE FUNCTION //
+showScoreMovieData()
+showActionMovieData()
+showAnimationMovieData()
+showDramaMovieData()
+showBestMovieData()
 
-// First Page
-    fetch(score_category[0]).then(res => res.json()).then(data=>{
-        Object.assign(score_url_obj1, data.results)})
-// Second Page
-    fetch(score_category[1]).then(res => res.json()).then(data=>{
-        Object.assign(score_url_obj2, data.results)})
+
+async function showBestMovieData(){
+    var bestScoreOneRes = await axios.get(score_category[0])
+    bestScoreRes = bestScoreOneRes.data.results.splice(0, 1)
+    console.log(bestScoreRes)
         
-// append data results of each url in an object
-data_obj = {score_url_obj1,score_url_obj2}
-// console.log(data_obj)
+    bestScoreRes.map(function (cur, index) {
+        bestMovie.insertAdjacentHTML(
+            "beforeend",
+             `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+        )
+    })
+}
+
+// CREATE THE SCORE FUNCTION
 
 
-
-
-
-// BEST SCORE MOVIEs CATEGORY
-
-fetch(score_category[0]).then(res => res.json()).then(data=>{
-    data_list.push(data.results);
-    main0.innerHTML = '';
-    data.results.forEach(movie => {
-        const {title, image_url, imdb_score, actors} = movie;
-        const movieE1 = document.createElement('div');
-        movieE1.classList.add('best_movie');
-        movieE1.innerHTML = `<button id="myBtn">
-        <img src="${image_url}" alt="Image_best">
-
-        <div class="best_movie-info">
-        
-            <h3>${title}</h3>
-            <span class="green">${imdb_score}</span>
-            
-        </div>
-        </button>
-        <div id="myModal" class="modal">
-        <div class="modal-content">
-        <span class="close">x</span>
-        <p>ACTORS: <br> ${actors}</p>
-        </div>
-        </div>         
-        `
-        main0.appendChild(movieE1)
-        click_button()
-        data_list[1].length = 1
-        })
+async function showScoreMovieData(){
+    
+    var scoreOneRes = await axios.get(score_category[0])
+    var scoreTwoRes = await axios.get(score_category[1])
+    scoreRes = scoreOneRes.data.results.concat(scoreTwoRes.data.results).splice(0,7)
+    console.log(scoreRes)
+    
+    scoreRes.map(function (cur, index) {
+        scoreSliders.insertAdjacentHTML(
+            "beforeend",
+             `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+        )
     })
 
-data_list = []        
-for (let x = 0; x < score_category.length; x++) {
-    x_url = score_category[x];
-    fetch(x_url).then(res => res.json()).then(data=>{
-        data_list.push(data.results);
-        main1.innerHTML = '';
-        data_list[0].forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE1 = document.createElement('div');
-            movieE1.classList.add('movie');
-            movieE1.innerHTML = `
-            <img src="${image_url}" alt="Image">
+    scrollPerClick = document.querySelector('.img-1').clientWidth + ImagePadding;
+}
 
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-            
-            `
 
-            main1.appendChild(movieE1)
-            })
-        data_list[1]?.forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE1 = document.createElement('div');
-            movieE1.classList.add('movie');
-            movieE1.innerHTML = `
-        
+// CALL THE SCORE SLIDER BUTTON FUNCTION
 
-            <img src="${image_url}" alt="Image">
+var scrollAmount = 0;
 
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div> 
-            `
-            main1.appendChild(movieE1)
-            data_list[1].length = 2
-            		// Call the modal box when the button is clicked.
-            })
+function sliderScrollLeft(){
+    scoreSliders.scrollTo({
+        top: 0,
+        left: (scrollAmount -= scrollPerClick),
+        behavior: "smooth"
+    });
+
+    if (scrollAmount < 0) {
+        scrollAmount = 0;  
+    }
+}
+function sliderScrollRight(){
+    if (scrollAmount <= scoreSliders.scrollWidth - scoreSliders.clientWidth) {
+        scoreSliders.scrollTo({
+            top:0,
+            left: (scrollAmount += scrollPerClick),
+            behavior: "smooth",    
+        });
+    }
+}
+
+
+// CREATE THE SCORE FUNCTION
+
+
+async function showActionMovieData(){
     
-        })
+    var actionOneRes = await axios.get(action_category[0])
+    var actionTwoRes = await axios.get(action_category[1])
+    actionRes =  actionOneRes.data.results.concat(actionTwoRes.data.results).splice(0,7)
+    
+    actionRes.map(function (cur, index) {
+        actionSliders.insertAdjacentHTML(
+            "beforeend",
+             `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+        )
+    })
+
+    scrollPerClick = document.querySelector('.img-1').clientWidth + ImagePadding;
 }
 
 
-// ACTION MOVIE CATEGORY
+// CALL THE SCORE SLIDER BUTTON FUNCTION
 
+var actionScrollAmount = 0;
 
-data_list2 = []
-for (let x = 0; x < action_category.length; x++) {
-    x_url = action_category[x];
-    fetch(x_url).then(res => res.json()).then(data=>{
-        data_list2.push(data.results);
-        main2.innerHTML = '';
-        data_list2[0].forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_action');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_action">
+function ActionsliderScrollLeft(){
+    actionSliders.scrollTo({
+        top: 0,
+        left: (actionScrollAmount -= scrollPerClick),
+        behavior: "smooth"
+    });
 
-            <div class="movie_action-info">
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-                 
-            `
-            main2.appendChild(movieE2)
-            })
-        data_list2[1]?.forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_action');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_action">
-
-            <div class="movie_action-info">
-
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-                 
-            `
-
-            main2.appendChild(movieE2)
-            data_list2[1].length = 2
-            		// Call the modal box when the button is clicked.
-            })
-        })
-}
-
-
-
-// BIOGRAPHY MOVIE CATEGORY
-
-
-data_list3 = []
-for (let x = 0; x < bio_category.length; x++) {
-    x_url = bio_category[x];
-    fetch(x_url).then(res => res.json()).then(data=>{
-        data_list3.push(data.results);
-        main3.innerHTML = '';
-        data_list3[0].forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_biography');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_biography">
-
-            <div class="movie_biography-info">
-
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-                 
-            `
-
-            main3.appendChild(movieE2)
-            })
-        data_list3[1]?.forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_biography');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_biography">
-
-            <div class="movie_biography-info">
-            
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-                  
-            `
-
-            main3.appendChild(movieE2)
-            data_list3[1].length = 2
-            		// Call the modal box when the button is clicked.
-            })
-        
-        })
-}
-
-
-// ANIMATIONs MOVIE CATEGORY
-
-
-data_list4 = []
-for (let x = 0; x < animation_category.length; x++) {
-    x_url = animation_category[x];
-    fetch(x_url).then(res => res.json()).then(data=>{
-        data_list4.push(data.results);
-        main4.innerHTML = '';
-        data_list4[0].forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_animation');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_animation">
-
-            <div class="movie_animation-info">
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>
-            
-            `
-
-            main4.appendChild(movieE2)
-            })
-        data_list4[1]?.forEach(movie => {
-            const {title, image_url, imdb_score, actors} = movie;
-            const movieE2 = document.createElement('div');
-            movieE2.classList.add('movie_animation');
-            movieE2.innerHTML = `
-            <img src="${image_url}" alt="Image_animation">
-
-            <div class="movie_animation-info">
-                <h3>${title}</h3>
-                <span class="green">${imdb_score}</span>
-            </div>  
-            `
-
-            main4.appendChild(movieE2)
-            data_list4[1].length = 2
-            		// Call the modal box when the button is clicked.
-            })
-        })
-}
-
-
-
-
-
-function click_button(){
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-                // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
+    if (actionScrollAmount < 0) {
+        actionScrollAmount = 0;  
     }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
+}
+function ActionsliderScrollRight(){
+    if (actionScrollAmount <= actionSliders.scrollWidth - actionSliders.clientWidth) {
+        actionSliders.scrollTo({
+            top:0,
+            left: (actionScrollAmount += scrollPerClick),
+            behavior: "smooth",    
+        });
     }
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-        modal.style.display = "none";
-        }
-        }   
+
+
+// CREATE ANIMATION 
+
+
+
+async function showAnimationMovieData(){
+    
+    var animationOneRes = await axios.get(animation_category[0])
+    var animationTwoRes = await axios.get(animation_category[1])
+    animationRes =  animationOneRes.data.results.concat(animationTwoRes.data.results).splice(0,7)
+    
+    animationRes.map(function (cur, index) {
+        animationSliders.insertAdjacentHTML(
+            "beforeend",
+             `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+        )
+    })
+
+    scrollPerClick = document.querySelector('.img-1').clientWidth + ImagePadding;
+}
+
+
+// CALL THE SCORE SLIDER BUTTON FUNCTION
+
+var animationScrollAmount = 0;
+
+function AnimationsliderScrollLeft(){
+    animationSliders.scrollTo({
+        top: 0,
+        left: (animationScrollAmount -= scrollPerClick),
+        behavior: "smooth"
+    });
+
+    if (animationScrollAmount < 0) {
+        animationScrollAmount = 0;  
+    }
+}
+function AnimationsliderScrollRight(){
+    if (animationScrollAmount <= animationSliders.scrollWidth - animationSliders.clientWidth) {
+        animationSliders.scrollTo({
+            top:0,
+            left: (animationScrollAmount += scrollPerClick),
+            behavior: "smooth",    
+        });
+    }
+}
+
+
+// CREATE DRAMA CATEGORY CAROUSEL
+
+
+
+async function showDramaMovieData(){
+    
+    var dramaOneRes = await axios.get(drama_category[0])
+    var dramaTwoRes = await axios.get(drama_category[1])
+    dramaRes =  dramaOneRes.data.results.concat(dramaTwoRes.data.results).splice(0,7)
+    
+    dramaRes.map(function (cur, index) {
+        dramaSliders.insertAdjacentHTML(
+            "beforeend",
+             `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+        )
+    })
+
+    scrollPerClick = document.querySelector('.img-1').clientWidth + ImagePadding;
+}
+
+
+// CALL THE SCORE SLIDER BUTTON FUNCTION
+
+var dramaScrollAmount = 0;
+
+function DramasliderScrollLeft(){
+    dramaSliders.scrollTo({
+        top: 0,
+        left: (dramaScrollAmount -= scrollPerClick),
+        behavior: "smooth"
+    });
+
+    if (dramaScrollAmount < 0) {
+        dramaScrollAmount = 0;  
+    }
+}
+function DramasliderScrollRight(){
+    if (dramaScrollAmount <= dramaSliders.scrollWidth - dramaSliders.clientWidth) {
+        dramaSliders.scrollTo({
+            top:0,
+            left: (dramaScrollAmount += scrollPerClick),
+            behavior: "smooth",    
+        });
+    }
 }
