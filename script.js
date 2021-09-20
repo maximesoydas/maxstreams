83d33a09a473a5a040e233db3fda2f8ec66b388b
Maxime Soydas <max.soydas@gmail.com>
2021-09-20 20:41:00 +0200

    fix(description):added description to modal

--- script.js
+++ script.js
@@ -16,28 +16,27 @@ const drama_category = []
 // APPEND CATEGORIES URLS TO THE CORRESPONDING CATEGORIES LIST
 for (let i = 1; i < 3; i++) {
     //Best Score Category
-    let SCORE_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=+-imdb_score&title=&title_contains=&writer=&writer_contains=&year=`;
+    // http://127.0.0.1:8000/api/v1/titles/?page=2&sort_by=-imdb_score
+    let SCORE_URL = `http://127.0.0.1:8000/api/v1/titles/?page=${i}&sort_by=-imdb_score`;
     score_category.push(SCORE_URL)
     // Animation Category
-    let ANIMATION_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Animation&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
+    let ANIMATION_URL = `http://127.0.0.1:8000/api/v1/titles/?genre=animation&page=${i}`;
     animation_category.push(ANIMATION_URL)
     // Action Category
-    let ACTION_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Action&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
+    let ACTION_URL = `http://127.0.0.1:8000/api/v1/titles/?genre=action&page=${i}`;
     action_category.push(ACTION_URL)
     // Biograhy Category
-    let DRAMA_URL = `http://127.0.0.1:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=Drama&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=${i}&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year=`;
+    let DRAMA_URL = `http://127.0.0.1:8000/api/v1/titles/?genre=drama&page=${i}`;
     drama_category.push(DRAMA_URL)
 }    
 
-
-
 // CALL THE FUNCTION FOR EACH CATEGORY TO RETURN DATA IN CAROUSEL
 
-ShowCategoryData(score_category,'scoreRes',scoreSliders, 7);
-ShowCategoryData(action_category, 'actionRes', actionSliders, 7);
-ShowCategoryData(animation_category, 'animationRes', animationSliders, 7);
-ShowCategoryData(drama_category, 'dramaRes', dramaSliders, 7);
-ShowCategoryData(score_category, 'bestScoreRes', bestMovie, 1);
+ShowCategoryData(score_category,scoreSliders, 7);
+ShowCategoryData(action_category, actionSliders, 7);
+ShowCategoryData(animation_category, animationSliders, 7);
+ShowCategoryData(drama_category, dramaSliders, 7);
+ShowCategoryData(score_category, bestMovie, 1);
 
 // DEFINE THE AMOUNT OF SCROLL PER CLICK ON THE CAROUSEL BUTTONS
 var ImagePadding = 300;
@@ -45,65 +44,37 @@ var ImagePadding = 300;
 // CALLS EACH MOVIE OF A GIVEN CATEGORY AND RETURNS EACH COVER PICTURE TO ITS CAROUSEL
 // ALSO ADDS ONCLICK FUNCTION ON COVER PICTURE WHICH CALLS A MODAL CONTAINING THE MOVIE DATA
 
-async function ShowCategoryData(category,category_name,div,num_movies) {
+async function ShowCategoryData(category,div,num_movies) {
     // USE AXIOS TO 'FETCH' DATE FROM TWO URLS
     var resultOne = await axios.get(category[0])
     var resultTwo = await axios.get(category[1])
-    // ADD MOVIES TO ITS CATEGORIES
-    if(category_name == 'bestScoreRes'){
-        bestScoreRes = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
-        bestScoreRes.map(async function (cur, index) {
-            div.insertAdjacentHTML(
-                "beforeend",
-                ` 
-                <img class="img-${index} slider-img" src="${cur.image_url}" onclick="movieDetailsModal(bestScoreRes, ${index})" />`
+
+    movie_list = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
+
+    movie_list.map(function (movie, index) {
+
+        div.insertAdjacentHTML(
+            "beforeend",
+            ` 
+            <img class="img-${index} slider-img" src="${movie.image_url}" />`
             )
-        })
-    }
-    else if(category_name == 'scoreRes'){
-        scoreRes = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
-        scoreRes.map(async function (cur, index) {
-            div.insertAdjacentHTML(
-                "beforeend",
-                ` 
-                <img class="img-${index} slider-img" src="${cur.image_url}" onclick="movieDetailsModal(scoreRes, ${index})" />`
-            )
-        })
-    }
-    else if(category_name == 'actionRes'){
-        actionRes = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
-        actionRes.map(async function (cur, index) {
-            div.insertAdjacentHTML(
-                "beforeend",
-                ` 
-                <img class="img-${index} slider-img" src="${cur.image_url}" onclick="movieDetailsModal(actionRes, ${index})" />`
-            )
-        })
-    }
-    else if(category_name == 'animationRes'){
-        animationRes = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
-        animationRes.map(async function (cur, index) {
-            div.insertAdjacentHTML(
-                "beforeend",
-                ` 
-                <img class="img-${index} slider-img" src="${cur.image_url}" onclick="movieDetailsModal(animationRes, ${index})" />`
-            )
-        })
-    }
-    else if(category_name == 'dramaRes'){
-        dramaRes = resultOne.data.results.concat(resultTwo.data.results).splice(0,num_movies)
-        dramaRes.map(async function (cur, index) {
-            div.insertAdjacentHTML(
-                "beforeend",
-                ` 
-                <img class="img-${index} slider-img" src="${cur.image_url}" onclick="movieDetailsModal(dramaRes, ${index})" />`
-            )
-        })
-    }
+            getMovieData(movie.url,div,index)
+    })
     // DEFINE THE QUERY SELECTOR OF THE CAROUSEL AND ADD THE PADDING FOR EACH CLICK
     scrollPerClick = document.querySelector('.img-1').clientWidth + ImagePadding;
 } 
 
+async function getMovieData(url_movie,div,index){
+
+    var movie = await axios.get(url_movie)
+    console.log(movie.data)
+    let selector = `.img-${index}`
+    // console.log(selector)
+    let movieImage = div.querySelector(selector)
+    movieImage.addEventListener("click", function(){
+        movieDetailsModal(movie.data)
+    });
+}
 
 
 function sliderScrollLeft(div,scrollAmount) {
@@ -132,10 +103,6 @@ function sliderScrollRight(div,scrollAmount) {
 }    
 
 
-
-
-
-
 // GET MODAL
 var modal = document.getElementById("myModal");
 var moveTitle = document.getElementById("movieTitle");
@@ -145,17 +112,17 @@ var moveScore = document.getElementById("movieScore");
 var moveDirector = document.getElementById("movieDirector");
 var moveActors = document.getElementById("movieActors");
 
-// var moveRated = document.getElementById("movieRated");
-// var moveLength = document.getElementById("movieLength");
-// var moveCountry = document.getElementById("movieCountry");
-// var moveMoney = document.getElementById("movieMoney");
-// var moveStoryline = document.getElementById("movieStoryline");
+var moveRated = document.getElementById("movieRated");
+var moveLength = document.getElementById("movieLength");
+var moveCountry = document.getElementById("movieCountry");
+var moveMoney = document.getElementById("movieMoney");
+var moveStoryline = document.getElementById("movieStoryline");
+var moveImage = document.getElementById('movieImage')
 // class=ipc-overflowText ipc-overflowText--pageSection ipc-overflowText--height-long ipc-overflowText--long ipc-overflowText--base
 
 
-function movieDetailsModal(movies, index) {
+function movieDetailsModal(movie) {
     //alert('Detail modal ...')
-    const movie = movies[index]
     console.log(movie)
     console.log(moveTitle)
 
@@ -165,7 +132,12 @@ function movieDetailsModal(movies, index) {
     moveScore.textContent = movie.imdb_score
     moveDirector.textContent = movie.directors
     moveActors.textContent = movie.actors
-
+    moveRated.textContent = movie.avg_vote
+    moveStoryline.textContent = movie.description
+    moveLength.textContent = movie.duration + ' Minutes'
+    moveCountry.textContent = movie.countries
+    moveMoney.textContent = movie.budget + ' ' + movie.budget_currency
+    moveImage.innerHTML = `<center> <img src='${movie.image_url}' /></center>`
     modal.style.display = "block";
 }    
 
@@ -180,3 +152,6 @@ window.onclick = function (event) {
         closeModal();        
     }    
 }    
+
+
+//TODO W3C
